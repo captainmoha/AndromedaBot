36001d517e053d40a327f493f45c15cda6808d88
Mohamed Ali Farouk <admiralmoha@live.com>
2016-12-18 08:37:20 +0000

    add reply feature with the help of  movies database

--- echoserver.py
+++ echoserver.py
@@ -1,9 +1,18 @@
 from flask import Flask, request
 import json
 import requests
-
+import string
+import sqlite3
+import random
+import os
 
 app = Flask(__name__)
+
+# database
+DB = 'movies.db'
+db_con = sqlite3.connect(DB, check_same_thread=False)
+cursor = db_con.cursor()
+
 
 # This needs to be filled with the Page Access Token that will be provided
 # by the Facebook App that will be created.
@@ -55,6 +64,10 @@ def messaging_events(payload):
   				yield event['sender']['id'], event['message']['text']
 
   			# cases other than text
+
+  			# else if 'attachments' in event['message']:
+  			# 	yield event['sender']['id'], event['message']['attachments']
+  				
   			else:
   				yield event['sender']['id'], 'u wot m8?'
 
@@ -70,7 +83,7 @@ def send_text_message(token, recipient, text):
  	print "id: " + str(recipient)
  	data = json.dumps({
  		'recipient': {'id': recipient},
- 		'message': {'text': text.decode('unicode_escape')}
+ 		'message': {'text': get_reply(text).decode('unicode_escape')}
  		})
 
  	headers = {'Content-type': 'application/json'}
@@ -80,6 +93,32 @@ def send_text_message(token, recipient, text):
  	if req.status_code != requests.codes.ok:
  		print "failed to send message: " + req.text
 
+def get_reply(text):
+	text = string.lower(text)
+	cursor.execute("SELECT line_id, text FROM LineSearch WHERE text Match ? AND text LIKE ?", ('^'+text+'$', '%'+text+'%'))
+	rows = cursor.fetchall()
+	for row in rows:
+		print("id: %s - text: %s" % (row[0], row[1]))
+		reply_id = int(row[0]) + 1
+		print("reply id: " + str(reply_id))
+		cursor.execute('SELECT text FROM LineSearch WHERE line_id = ?', (reply_id,))
+		reply = cursor.fetchone()
+		break
+	print(str(len(rows)))
+
+	
+
+	
+	if (reply == None):
+		return "Let's talk about something else..."
+
+	print("reply: %s" % reply[0])
+
+	return reply[0]
+
+
+get_reply('what do you do for fun')
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
   			
