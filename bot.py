80c6aeeca8d1bb84f843fa73a572e6827ac6ab0d
captainmoha <admiralmoha@gmail.com>
2016-12-24 10:10:16 +0200

    add weather

--- bot.py
+++ bot.py
@@ -350,11 +350,14 @@ def get_weather_json(city):
 
 	# first api call to get city id
 	current_weather = requests.get(CURRENT_WEATHER_API, params={'q': city, 'appid': WEATHER_API_KEY, 'units': 'metric'})
-	city_id = dict(current_weather).get('id', -1)
 
-	# second api call to get weather json
-	current_weather = requests.get(DAILY_WEATHER_API, params={'id':city_id, 'appid': WEATHER_API_KEY, 'units': 'metric', 'cnt': '1'})
-	print("Weather " + str(current_weather))
+	if (current_weather and len(current_weather.json()) > 2):
+		city_id = current_weather.json()['id']
+		
+		# second api call to get weather json
+		current_weather = requests.get(DAILY_WEATHER_API, params={'id':city_id, 'appid': WEATHER_API_KEY, 'units': 'metric', 'cnt': '1'})
+		print("Weather " + str(current_weather))
+
 	return current_weather.json()
 	
 
